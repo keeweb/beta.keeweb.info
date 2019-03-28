2fe7b4c75389bdfb55c24acaa0133b252677d5a2
antelle <dmitryvit@gmail.com>
2019-03-28 23:36:16 +0100

    beta

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
-});
